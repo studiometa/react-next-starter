e215e0ca526fef7107643bae3503895cd9d196ad
chuck-durst <chuckdurst67@gmail.com>
2018-04-29 17:48:49 +0200

    Add autoprefixer module to the postcss module

--- next.config.js
+++ next.config.js
@@ -3,13 +3,18 @@ const withSass = require('@zeit/next-sass');
 
 module.exports = withSass({
  cssModules: true,
-  distDir: '..//build',
+  distDir: '../build',
   cssLoaderOptions: {
     importLoaders: 1,
     localIdentName: "[local]",
   },
 
   webpack: (config, { buildId, dev, isServer, defaultLoaders }) => {
+   console.log(config.module.rules[2].use[3])
+    config.module.rules.push({
+      test: /\.css$/,
+      use: ["style-loader", "css-loader", "postcss-loader"]
+    });
     return config
   },
 
