b3e36208397640c00e24dbf520f4e997bcdd9006
chuck-durst <chuckdurst67@gmail.com>
2018-12-17 17:35:04 +0100

    Update readme

--- client/pages/readme.js
+++ client/pages/readme.js
@@ -5,14 +5,44 @@ import pageWrapper from '../lib/pageWrapper';
 
 const readme = require('../../README.md');
 
+const styles = theme => ({
+  mdContent: {
+    fontFamily: theme.typography.fontFamily,
+    '&> h1': {
+      marginBottom: theme.spacing.unit * 8,
+    },
+    '&> h2': {
+      marginBottom: theme.spacing.unit * 4,
+      marginTop: theme.spacing.unit * 6,
+    },
+    '&> h3': {
+      marginTop: theme.spacing.unit * 4,
+    },
+    '& * code, & pre': {
+      background: '#fff',
+      padding: 2,
+    },
+    '& p, & * p, & * li': {
+      'line-height': '1.5em'
+    },
+    '& table, & * table': {
+      borderCollapse: 'collapse',
+      border: `1px solid ${theme.palette.grey[200]}`,
+      '& td, & tr, & th': {
+        border: `1px solid ${theme.palette.grey[200]}`,
+        padding: theme.spacing.unit / 2
+      }
+    }
+  },
+})
 
 const Readme = class extends React.Component {
 
   render() {
-
+const { classes } = this.props;
     return (
       <Layout title={this.props.t('menu_links.readme')} pageData={this.props.pageData}>
-        <div dangerouslySetInnerHTML={{ __html: readme }}/>
+        <div dangerouslySetInnerHTML={{ __html: readme }} className={classes.mdContent}/>
       </Layout>
     );
   }
@@ -22,6 +52,7 @@ const mapStateToProps = state => ({});
 
 export default pageWrapper(Readme, {
   mapStateToProps,
+  styles,
   name: 'readme',
   noPageData: true
 });
