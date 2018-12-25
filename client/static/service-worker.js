76caedc96057e674736576c86ea808d38e9c6092
chuck-durst <chuckdurst67@gmail.com>
2018-12-25 14:12:41 +0100

    Perform a 301 redirection when an url ends with a slash

--- client/static/service-worker.js
+++ client/static/service-worker.js
@@ -1,78 +1,78 @@
 self.__precacheManifest = [
   {
-    "url": "/_next/static/Ww45VC9utVnqT50qrbaOC/pages/_doc/router.js"
+    "url": "/_next/static/seZv6cQ5_5z0ftMrvjUvB/pages/_doc/performances.js"
   },
   {
-    "url": "/_next/static/chunks/commons.f643062badde09274c68.js"
+    "url": "/_next/static/chunks/commons.ec8ced33b73ebc0e72bf.js"
   },
   {
     "url": "/_next/static/runtime/main-06f9893ba7f4fc52765c.js"
   },
   {
+    "url": "/_next/static/css/styles.1b708ff3.chunk.css"
+  },
+  {
     "url": "/_next/static/chunks/styles.3621f366cd158e0a9690.js"
   },
   {
-    "url": "/_next/static/css/styles.1b708ff3.chunk.css"
+    "url": "/_next/static/seZv6cQ5_5z0ftMrvjUvB/pages/_app.js"
+  },
+  {
+    "url": "/_next/static/seZv6cQ5_5z0ftMrvjUvB/pages/_doc/api.js"
+  },
+  {
+    "url": "/_next/static/seZv6cQ5_5z0ftMrvjUvB/pages/_doc/components.js"
+  },
+  {
+    "url": "/_next/static/seZv6cQ5_5z0ftMrvjUvB/pages/_doc/configuration.js"
+  },
+  {
+    "url": "/_next/static/seZv6cQ5_5z0ftMrvjUvB/pages/_doc/discover.js"
+  },
+  {
+    "url": "/_next/static/seZv6cQ5_5z0ftMrvjUvB/pages/_doc/get-started.js"
+  },
+  {
+    "url": "/_next/static/seZv6cQ5_5z0ftMrvjUvB/pages/_doc/i18n.js"
+  },
+  {
+    "url": "/_next/static/seZv6cQ5_5z0ftMrvjUvB/pages/_doc/intro.js"
   },
   {
     "url": "/_next/static/runtime/webpack-89179faa512dd01fbb62.js"
   },
   {
-    "url": "/_next/static/Ww45VC9utVnqT50qrbaOC/pages/_doc/troubleshooting.js"
+    "url": "/_next/static/seZv6cQ5_5z0ftMrvjUvB/pages/_doc/requirements.js"
   },
   {
-    "url": "/_next/static/Ww45VC9utVnqT50qrbaOC/pages/_doc/configuration.js"
+    "url": "/_next/static/seZv6cQ5_5z0ftMrvjUvB/pages/_doc/router.js"
   },
   {
-    "url": "/_next/static/Ww45VC9utVnqT50qrbaOC/pages/_doc/get-started.js"
+    "url": "/_next/static/seZv6cQ5_5z0ftMrvjUvB/pages/_doc/store.js"
   },
   {
-    "url": "/_next/static/Ww45VC9utVnqT50qrbaOC/pages/_doc/i18n.js"
+    "url": "/_next/static/seZv6cQ5_5z0ftMrvjUvB/pages/_doc/theme.js"
   },
   {
-    "url": "/_next/static/Ww45VC9utVnqT50qrbaOC/pages/_doc/intro.js"
+    "url": "/_next/static/seZv6cQ5_5z0ftMrvjUvB/pages/_doc/troubleshooting.js"
   },
   {
-    "url": "/_next/static/Ww45VC9utVnqT50qrbaOC/pages/_doc/performances.js"
+    "url": "/_next/static/seZv6cQ5_5z0ftMrvjUvB/pages/_doc/under-the-hood.js"
   },
   {
-    "url": "/_next/static/Ww45VC9utVnqT50qrbaOC/pages/_doc/requirements.js"
+    "url": "/_next/static/seZv6cQ5_5z0ftMrvjUvB/pages/_doc/wrappers.js"
   },
   {
-    "url": "/_next/static/Ww45VC9utVnqT50qrbaOC/pages/readme.js"
+    "url": "/_next/static/seZv6cQ5_5z0ftMrvjUvB/pages/_error.js"
   },
   {
-    "url": "/_next/static/Ww45VC9utVnqT50qrbaOC/pages/_doc/store.js"
+    "url": "/_next/static/seZv6cQ5_5z0ftMrvjUvB/pages/_sandbox.js"
   },
   {
-    "url": "/_next/static/Ww45VC9utVnqT50qrbaOC/pages/_doc/theme.js"
+    "url": "/_next/static/seZv6cQ5_5z0ftMrvjUvB/pages/index.js"
   },
   {
-    "url": "/_next/static/Ww45VC9utVnqT50qrbaOC/pages/_doc/discover.js"
-  },
-  {
-    "url": "/_next/static/Ww45VC9utVnqT50qrbaOC/pages/_doc/under-the-hood.js"
-  },
-  {
-    "url": "/_next/static/Ww45VC9utVnqT50qrbaOC/pages/_doc/wrappers.js"
-  },
-  {
-    "url": "/_next/static/Ww45VC9utVnqT50qrbaOC/pages/_error.js"
-  },
-  {
-    "url": "/_next/static/Ww45VC9utVnqT50qrbaOC/pages/_sandbox.js"
-  },
-  {
-    "url": "/_next/static/Ww45VC9utVnqT50qrbaOC/pages/index.js"
-  },
-  {
-    "url": "/_next/static/Ww45VC9utVnqT50qrbaOC/pages/_doc/components.js"
-  },
-  {
-    "url": "/_next/static/Ww45VC9utVnqT50qrbaOC/pages/_doc/api.js"
-  },
-  {
-    "url": "/_next/static/Ww45VC9utVnqT50qrbaOC/pages/_app.js"
+    "url": "/_next/static/seZv6cQ5_5z0ftMrvjUvB/pages/readme.js"
   }
 ];
 
@@ -92,14 +92,12 @@ self.__precacheManifest = [
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-
+  
 );
 
 workbox.skipWaiting();
 workbox.clientsClaim();
-workbox.setConfig({
-  debug: true
-});
+
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
  * requests for URLs in the manifest.
