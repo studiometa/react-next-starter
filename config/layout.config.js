2223e1f870c913a11f69c8558c5ea6ab79149301
chuck-durst <chuckdurst67@gmail.com>
2019-07-19 23:10:06 +0200

    Update doc

--- config/layout.config.js
+++
@@ -1,8 +0,0 @@
-/**
- * Some parameter about the global layout of your app
- */
-module.exports = {
-
-  // The max width of the page content before showing side gutters
-  pageContentMaxWidth: 1440,
-};
--- config/redux.config.js
+++ config/redux.config.js
@@ -4,5 +4,6 @@
 module.exports = {
 
   // items that should be stored in the local storage (ex: 'user.profile', etc)
+  // pageData fetched from the API are always stored locally but never cached
   localStorageStates: [],
 };
